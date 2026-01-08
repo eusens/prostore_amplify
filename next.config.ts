6b195a279dcbe343feceb6fe3683563dd9548ef0
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2026-01-08 12:31:31 +0800

    del hardcode

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // 添加这 3 行
   env: {
-    DATABASE_URL: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_ir2MwRYOu0Ue@ep-spring-cherry-ahpvvxhw-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 };
 
