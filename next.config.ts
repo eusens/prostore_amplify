d64cd273a6bb3814d2d0e797f32737df4397ed28
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2026-01-08 11:56:22 +0800

    fix: hardcode DATABASE_URL in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 添加这 3 行
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_ir2MwRYOu0Ue@ep-spring-cherry-ahpvvxhw-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  },
 };
 
 export default nextConfig;
