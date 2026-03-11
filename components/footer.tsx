e7c43c46f31b946939879c0cc3a04893dc9465d5
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2026-03-11 14:03:01 +0800

    edit footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className='border-t'>
       <div className='p-5 flex-center'>
-        {currentYear} {APP_NAME}. All Rights reserved.
+         {currentYear} {APP_NAME}. All Rights reserved.&nbsp;by&nbsp;<a href="https://eusens.com" target="_blank">Eusens Technology</a>
       </div>
     </footer>
   );
